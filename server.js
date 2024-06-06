066730cfbb85643225357feffa68aa656c748474
Sarthak Joshi <sarthaksj02@gmail.com>
2024-06-06 18:44:18 +0530

    new URL added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 const app = express();
 
-app.use(cors({ origin: `https://easyfit-4t0r07xen-sarthak-joshis-projects-e4cfa87c.vercel.app` }));
+app.use(cors({ origin: `https://easyfit-workouts.vercel.app/` }));
 
 app.use(express.json());
 
